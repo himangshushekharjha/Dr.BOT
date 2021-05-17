159984cb67cda9e9ca22aa101e1973c569b6a5ce
himancifer <himangshushekharjha@gmail.com>
2021-05-17 17:10:45 +0530

    Initial setup

---
+++ frontend/src/components/Landing.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Landing',
+
+    data: () => ({
+  
+    }),
+  }
+</script>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <landing />
+</template>
+
+<script>
+  import Landing from '../components/Landing'
+
+  export default {
+    name: 'Home',
+
+    components: {
+      Landing,
+    },
+  }
+</script>
